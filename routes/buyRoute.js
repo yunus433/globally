75742d1bc708722f07c8b52298b81a2f15f5fc41
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-09-28 23:00:27 +0300

    -

--- routes/buyRoute.js
+++ routes/buyRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedIn = require("../middleware/isLoggedin");
 
 const indexGetController = require("../controllers/buy/get");
 
