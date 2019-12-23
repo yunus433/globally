093f7f84abb8bed14026a1bf8a61fc3479926897
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-12-24 00:57:22 +0300

    Improve mobil design for the authentication page

--- public/js/auth/index.js
+++ public/js/auth/index.js
@@ -31,6 +31,18 @@ window.onload = () => {
         categoryWrapperResponsive.classList.remove("close-up-animation-class");
       }
     }
+
+    if (event.target.className == "right-side-open-button" || event.target.parentNode.className == "right-side-open-button") {
+      document.querySelector(".left-side-open-button").style.borderColor = "rgb(205, 204, 204)";
+      document.querySelector(".main-wrapper-left-side").style.display = "none";
+      document.querySelector(".right-side-open-button").style.borderColor = "rgb(136, 136, 136)";
+      document.querySelector(".main-wrapper-right-side").style.display = "flex";
+    } else if (event.target.className == "left-side-open-button" || event.target.parentNode.className == "left-side-open-button") {
+      document.querySelector(".right-side-open-button").style.borderColor = "rgb(205, 204, 204)";
+      document.querySelector(".main-wrapper-right-side").style.display = "none";
+      document.querySelector(".left-side-open-button").style.borderColor = "rgb(136, 136, 136)";
+      document.querySelector(".main-wrapper-left-side").style.display = "flex";
+    }
   });
 
   document.addEventListener("mouseover", (event) => {
