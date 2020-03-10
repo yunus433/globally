2e9121badcf3b60e40d013d0b6eec8e7678efb6e
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-03-10 08:30:04 +0300

    Add /admin/details route photo add and delete utility

--- public/js/admin/details.js
+++ public/js/admin/details.js
@@ -1,3 +1,25 @@
+function createNewProductPhoto(imageSrc) {  
+  const imageWrapper = document.createElement('div');
+  imageWrapper.classList.add('each-product-photo');
+  const image = document.createElement('img');
+  image.src= imageSrc;
+  const i = document.createElement('i');
+  i.classList.add('each-product-edit-icon');
+  i.classList.add('fas');
+  i.classList.add('fa-trash');
+  imageWrapper.appendChild(image);
+  imageWrapper.appendChild(i);
+
+  if (document.querySelector('.images-wrapper').childNodes.length >= 5) {
+    document.querySelector('.add-new-button').style.display = 'none';
+  }
+
+  document.querySelector('.images-wrapper').appendChild(imageWrapper);
+  document.querySelector('.images-wrapper').insertBefore(imageWrapper, imageWrapper.previousElementSibling);
+  document.querySelector('.images-outer-wrapper').scrollLeft =  document.querySelector('.images-outer-wrapper').scrollWidth;
+}
+
+
 window.onload = () => {
   const product = JSON.parse(document.getElementById("product").value);
   const categoryInputs = document.querySelectorAll(".category-input");
@@ -11,5 +33,58 @@ window.onload = () => {
   typeInputs.forEach(input => {
     if (input.value == product.type)
       input.checked = true;
+  });
+
+  const productPhotoInput = document.getElementById('product-image-input');
+  if (productPhotoInput)
+    productPhotoInput.onchange = (event) => {
+      const file = productPhotoInput.files[0];
+      var formdata = new FormData();
+      formdata.append('file', file);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", `/admin/details/photo/add?id=${product._id}`);
+      xhr.send(formdata);
+      
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.responseText){
+          if (xhr.status == 500) {
+            alert("Bir hata oluştu, lütfen tekrar deneyin.");
+            productPhotoInput.value = ''
+            if (!/safari/i.test(navigator.userAgent)){
+              productPhotoInput.type = ''
+              productPhotoInput.type = 'file'
+            }
+          } else {
+            createNewProductPhoto(xhr.responseText);
+            productPhotoInput.value = ''
+            if (!/safari/i.test(navigator.userAgent)){
+              productPhotoInput.type = ''
+              productPhotoInput.type = 'file'
+            }
+          }
+        };
+      };
+    };
+
+  document.addEventListener('click', (event) => {
+    if (event.target.className == 'each-product-edit-icon fas fa-trash') {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", `/admin/details/photo/delete?id=${product._id}`);
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.send(JSON.stringify({
+        "photo": event.target.parentNode.childNodes[0].src.toString()
+      }));
+      
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4){
+          if (xhr.status == 200) {
+            event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+          } else {
+            alert("Bir hata oluştu, lütfen tekrar deneyin.");
+          };
+        };
+      };
+    };
   });
 }
